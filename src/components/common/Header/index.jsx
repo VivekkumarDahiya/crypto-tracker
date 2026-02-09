0bf312733a469156378581c32e975db1321aa447
Vivek Kumar Dahiya <dahiyavivek933@gmail.com>
2026-02-09 09:56:12 +0530

    add header

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -3,9 +3,25 @@ import "./style.css"
 
 const Header=()=>{
     return(
-        <div className="header">
-         hello
-        </div>
+        <header className="header">
+
+           <h1  className="logo">CryptoTracker<span style={{color:"var(--blue)"}}>.</span></h1>
+           
+           <nav  className="links">
+           <a href="/" className="navlink">
+            <p className="link">Home</p>
+           </a>
+           <a href="/" className="navlink">
+            <p className="link">Compare</p>
+           </a>
+           <a href="/" className="navlink">
+            <p className="link">Watchlist</p>
+           </a >
+           <a href="/" className="navlink">
+            <p className="link">Dashboard</p>
+           </a>
+           </nav>
+        </header>
     )
 }
 
