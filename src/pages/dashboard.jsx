bec5f6899e16d6b52fb7019148f65f834f4253e7
Vivek Kumar Dahiya <dahiyavivek933@gmail.com>
2026-02-13 15:04:43 +0530

    pagination created

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,38 +1,64 @@
+
 import { useEffect, useState } from "react";
 import Header from "../components/common/Header";
 import TabComponents from "../components/dashboard/tabs";
-import axios from "axios"
+import axios from "axios";
 import Search from "../components/dashboard/searchBar";
+import PaginationComponent from "../components/dashboard/pagination";
 
+const Dashboard = () => {
+  const [search, setSearch] = useState("");
+  const [coins, setCoins] = useState([]);
+  const [paginatedcoins, setPaginatedCoins] = useState([]);
+  const [page, setPage] = useState(1);
 
-const Dashboard=()=>{
-     const [search,setSearch]=useState("")
-    const [coins,setCoins]=useState([])
+  const handlePageChange = (event, value) => {
+    setPage(value);
+    var previousIndex = (value - 1) * 10;
+    setPaginatedCoins(coins.slice(previousIndex, previousIndex + 10));
+  };
 
-    const onSearchChange=(e)=>{
-        setSearch(e.target.value)
-    }
+  const onSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-    var filteredCoins=coins.filter((item)=>
-    item.name.toLowerCase().includes(search.toLowerCase()) ||  item.symbol.toLowerCase().includes(search.toLowerCase()))
+  var filteredCoins = coins.filter(
+    (item) =>
+      item.name.toLowerCase().includes(search.toLowerCase()) ||
+      item.symbol.toLowerCase().includes(search.toLowerCase())
+  );
 
-useEffect(()=>{
-    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1")
-    .then((response)=>{
-          console.log("Response",response)
-          setCoins(response.data)
-    })
-    .catch((error)=>{
-          console.lop(error)
-    })
-},[])
-    return(
-        <div>
-         <Header/>
-         <Search  search={search} onSearchChange={onSearchChange}/>
-         <TabComponents coins={filteredCoins}/>
-        </div>
-    )
-}
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1"
+      )
+      .then((response) => {
+        setCoins(response.data);
+        setPaginatedCoins(response.data.slice(0, 10));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Search search={search} onSearchChange={onSearchChange} />
+
+      <TabComponents
+        coins={search ? filteredCoins : paginatedcoins}
+      />
+
+      {!search && (
+        <PaginationComponent
+          page={page}
+          handlePageChange={handlePageChange}
+        />
+      )}
+    </div>
+  );
+};
 
 export default Dashboard;
