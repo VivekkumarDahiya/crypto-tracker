63cd1fcb4074546f83df7764a234342f6f89c31a
Vivek Kumar Dahiya <dahiyavivek933@gmail.com>
2026-02-13 12:46:52 +0530

    search bar and Filter

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,11 +2,19 @@ import { useEffect, useState } from "react";
 import Header from "../components/common/Header";
 import TabComponents from "../components/dashboard/tabs";
 import axios from "axios"
+import Search from "../components/dashboard/searchBar";
 
 
 const Dashboard=()=>{
-
+     const [search,setSearch]=useState("")
     const [coins,setCoins]=useState([])
+
+    const onSearchChange=(e)=>{
+        setSearch(e.target.value)
+    }
+
+    var filteredCoins=coins.filter((item)=>
+    item.name.toLowerCase().includes(search.toLowerCase()) ||  item.symbol.toLowerCase().includes(search.toLowerCase()))
 
 useEffect(()=>{
     axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1")
@@ -21,7 +29,8 @@ useEffect(()=>{
     return(
         <div>
          <Header/>
-         <TabComponents coins={coins}/>
+         <Search  search={search} onSearchChange={onSearchChange}/>
+         <TabComponents coins={filteredCoins}/>
         </div>
     )
 }
